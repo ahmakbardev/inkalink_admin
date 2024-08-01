3ccbfeffb656a0a2417cd68a851cd3bec25ecf99
Ahmad Akbar Mubarak <130969627+ahmakbardev@users.noreply.github.com>
2024-08-01 18:45:55 +0700

    update footer

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer>
+{{-- <footer>
     <div class="px-6 border-t border-gray-300 py-3 flex justify-between items-center">
         <p class="m-0 leading-6">
             Made by
@@ -12,4 +12,4 @@
             </svg>
         </a>
     </div>
-</footer>
+</footer> --}}
